f705995a226407ac10d80238105a620bd4b91b20
Aurimas Šimkus <saurimas@hotmail.com>
2018-01-19 18:00:25 +0200

    Accepts name

--- ConsoleCalculator/ConsoleCalculator/Program.cs
+++ ConsoleCalculator/ConsoleCalculator/Program.cs
@@ -10,7 +10,11 @@ namespace ConsoleCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Mano pirma programa!");
+            Console.WriteLine("Mano pirma programa! Koks tavo vardas?");
+
+            string name = Console.ReadLine();
+            Console.WriteLine("Labas, " +                     name);
+
             Console.ReadLine(); 
         }
     }
