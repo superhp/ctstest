10dceeea6411711d9d12c740682a378754e6cfdc
Aurimas Šimkus <saurimas@hotmail.com>
2018-01-24 19:43:29 +0200

    Player enum implemented

--- TicTacToe/TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/TicTacToe/MainWindow.xaml.cs
@@ -30,8 +30,18 @@ namespace TicTacToe
 
         }
 
-        private bool _player = false; // false - x, true - 0
-        
+        private enum Player
+        {
+            X = 1, 
+            O = 2
+        }
+
+        private Player _player = Player.O; // false - x, true - 0
+        private int[,] _matrix = new int[,] { 
+            { 0, 0, 0 }, 
+            { 0, 0, 0 }, 
+            { 0, 0, 0 }
+        };
 
         private void buttonClicked(object sender, RoutedEventArgs e)
         {
@@ -39,10 +49,10 @@ namespace TicTacToe
             int x = int.Parse(coordinates[0].ToString());
             int y = int.Parse(coordinates[1].ToString());
 
-            _player = !_player;
+            _player = _player == Player.O ? Player.X : Player.O;
 
-            ((Button)sender).Content = _player ? "0" : "X";
-            ((Button)sender).Foreground = _player ? Brushes.Blue : Brushes.Red; 
+            ((Button)sender).Content = _player == Player.O ? "0" : "X";
+            ((Button)sender).Foreground = _player == Player.O ? Brushes.Blue : Brushes.Red; 
         }
     }
 }
