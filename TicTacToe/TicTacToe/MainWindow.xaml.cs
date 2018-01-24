ff545615216ae8c0a8e29c51976bd391f437b2da
Aurimas Šimkus <saurimas@hotmail.com>
2018-01-24 19:47:22 +0200

    Player extracted to a separate file

--- TicTacToe/TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/TicTacToe/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TicTacToe
 {
@@ -23,18 +12,7 @@ namespace TicTacToe
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private enum Player
-        {
-            X = 1, 
-            O = 2
-        }
+        }                  
 
         private Player _player = Player.O; // false - x, true - 0
         private int[,] _matrix = new int[,] { 
